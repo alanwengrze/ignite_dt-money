96e4bec0c93556cdea4b9a9378ace7f881c205d1
alan <alanwengrzee@gmail.com>
2024-12-10 22:29:59 -0300

    :bug: fix: url changed

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://dt-money-api-wo19.onrender.com/",
-  baseURL: "http://localhost:3333/",
+  baseURL: "https://dt-money-api-wo19.onrender.com/",
 });
